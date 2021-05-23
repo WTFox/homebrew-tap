8bb02e0355c067b5853d27440c74f8ca006baa6e
A. Fox <anthonyfox1988@gmail.com>
2021-05-22 19:58:03 -0700

    Brew formula update for jsonfind version v1.0.2

--- Formula/jsonfind.rb
+++ Formula/jsonfind.rb
@@ -11,28 +11,26 @@ class Jsonfind < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/wtfox/jsonfind/releases/download/v1.0.2/jsonfind_1.0.2_Darwin_x86_64.tar.gz"
-    sha256 "fdbe4a5da0becd2e927076b15516366f91f265496e64ad077f97ac55b424fbd4"
+    sha256 "c1208e7b1fedb15202f87c5dcd2aef87eac3d3c8fb9b391aaea332dcfdc24782"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/wtfox/jsonfind/releases/download/v1.0.2/jsonfind_1.0.2_Darwin_arm64.tar.gz"
-    sha256 "c9ec671c91dddadbe8090143bba0811fa79e019bcf43e887fa7a3b2c8ca4a9d5"
+    sha256 "8f134bfab052c79d6e9baa441c8bedce2cd18e2976c8bc7883603f48f4138593"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/wtfox/jsonfind/releases/download/v1.0.2/jsonfind_1.0.2_Linux_x86_64.tar.gz"
-    sha256 "d549f24b82495a0b8f90925bbafe473420c13a143fa0dbb0365223d55d712069"
+    sha256 "f6a369564cfbdeaf59ce0ba35e535d6a2c32a4d7003a9815bf313e2665e40795"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/wtfox/jsonfind/releases/download/v1.0.2/jsonfind_1.0.2_Linux_arm64.tar.gz"
-    sha256 "ea2f3d9ced3ff2579681af28d974dd8ecf80feddf9c56b3b2fd56b636a6248a1"
+    sha256 "902960769de0bed9fc6bf394c9e06c8347198349a5aaa9e7eec5378accc0e599"
   end
 
   def install
     bin.install "jsonfind"
-    ...
   end
 
   test do
     system "#{bin}/jsonfind --help"
-    ...
   end
 end
